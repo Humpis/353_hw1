a5395e4f3b96eda8fdf817f8801b4289e97a0af8
Vidar Minkovsky <vmsailor@gmail.com>
2017-04-21 20:08:56 -0400

    cleanup

--- knn.py
+++ knn.py
@@ -64,111 +64,87 @@ metric = -1
 
 if(len(sys.argv) == 6):
     if sys.argv[2] == "--punct":
-        # print("Checking for punctuation-included words file...")
-        # if os.path.isfile(os.getcwd() + '/punctWords.txt'):
-        #     print("Punctiation-included words file already exists. Loading...")
-        #     with open(os.getcwd() + '/punctWords.txt', 'r') as f:
-        #         try:
-        #             words = json.load(f)
-        #             # if the file is empty the ValueError will be thrown
-        #         except ValueError:
-        #             words = {}
-        #             print("File is empty error")
-        #             sys.exit(1)
-        # else:
-        #    print("Punctiation-included words file not found. Generating...")
-            print("Generating list of words...")
-            for filename in os.listdir(path_neg):
-                with open(path_neg + filename, 'r') as f:
-                    for line in f:
-                        for word in line.split():
-                            if word not in words:
-                                # Stop words
-                                if(sys.argv[5] == '--stopwords'):
+        print("Generating list of words...")
+        for filename in os.listdir(path_neg):
+            with open(path_neg + filename, 'r') as f:
+                for line in f:
+                    for word in line.split():
+                        if word not in words:
+                            # Stop words
+                            if(sys.argv[5] == '--stopwords'):
+                                words[word] = counter
+                                counter += 1
+                            elif(sys.argv[5] == '--nostopwords'):
+                                if word not in stopwords:
                                     words[word] = counter
                                     counter += 1
-                                elif(sys.argv[5] == '--nostopwords'):
-                                    if word not in stopwords:
-                                        words[word] = counter
-                                        counter += 1
-                                else:
-                                    print("Invalid argument")
-                                    sys.exit(1)
-            for filename in os.listdir(path_pos):
-                with open(path_pos + filename, 'r') as f:
-                    for line in f:
-                        for word in line.split():
-                            if word not in words:
-                                # Stop words
-                                if(sys.argv[5] == '--stopwords'):
+                            else:
+                                print("Invalid argument")
+                                sys.exit(1)
+        for filename in os.listdir(path_pos):
+            with open(path_pos + filename, 'r') as f:
+                for line in f:
+                    for word in line.split():
+                        if word not in words:
+                            # Stop words
+                            if(sys.argv[5] == '--stopwords'):
+                                words[word] = counter
+                                counter += 1
+                            elif(sys.argv[5] == '--nostopwords'):
+                                if word not in stopwords:
                                     words[word] = counter
                                     counter += 1
-                                elif(sys.argv[5] == '--nostopwords'):
-                                    if word not in stopwords:
-                                        words[word] = counter
-                                        counter += 1
-                                else:
-                                    print("Invalid argument")
-                                    sys.exit(1)
-            # words_file = open('punctNoStopWords.txt', 'w')
-            # json.dump(words, words_file)
-            # print("Generation sucsessful")
+                            else:
+                                print("Invalid argument")
+                                sys.exit(1)
+        # words_file = open('punctNoStopWords.txt', 'w')
+        # words_file = open('punctWords.txt', 'w')
+        # json.dump(words, words_file)
+        # print("Generation sucsessful")
     elif sys.argv[2] == "--nopunct":
-        # print("Checking for punctuation-excluded words file...")
-        # if os.path.isfile(os.getcwd() + '/nopunctWords.txt'):
-        #     print("Punctiation-excluded words file already exists. Loading...")
-        #     with open(os.getcwd() + '/nopunctWords.txt', 'r') as f:
-        #         try:
-        #             words = json.load(f)
-        #             # if the file is empty the ValueError will be thrown
-        #         except ValueError:
-        #             words = {}
-        #             print("File is empty error")
-        #             sys.exit(1)
-        # else:
-        #     print("Punctiation-excluded words file not found. Generating...")
-            print("Generating list of words...")
-            for filename in os.listdir(path_neg):
-                with open(path_neg + filename, 'r') as f:
-                    for line in f:
-                        for word in line.split():
-                            newWord = re.sub(
-                                '[' + string.punctuation + ']', '', word)
-                            if newWord != '':
-                                if word not in words:
-                                    # Stop words
-                                    if(sys.argv[5] == '--stopwords'):
+        print("Generating list of words...")
+        for filename in os.listdir(path_neg):
+            with open(path_neg + filename, 'r') as f:
+                for line in f:
+                    for word in line.split():
+                        newWord = re.sub(
+                            '[' + string.punctuation + ']', '', word)
+                        if newWord != '':
+                            if word not in words:
+                                # Stop words
+                                if(sys.argv[5] == '--stopwords'):
+                                    words[word] = counter
+                                    counter += 1
+                                elif(sys.argv[5] == '--nostopwords'):
+                                    if word not in stopwords:
                                         words[word] = counter
                                         counter += 1
-                                    elif(sys.argv[5] == '--nostopwords'):
-                                        if word not in stopwords:
-                                            words[word] = counter
-                                            counter += 1
-                                    else:
-                                        print("Invalid argument")
-                                        sys.exit(1)
-            for filename in os.listdir(path_pos):
-                with open(path_pos + filename, 'r') as f:
-                    for line in f:
-                        for word in line.split():
-                            newWord = re.sub(
-                                '[' + string.punctuation + ']', '', word)
-                            if newWord != '':
-                                if word not in words:
-                                    # Stop words
-                                    if(sys.argv[5] == '--stopwords'):
+                                else:
+                                    print("Invalid argument")
+                                    sys.exit(1)
+        for filename in os.listdir(path_pos):
+            with open(path_pos + filename, 'r') as f:
+                for line in f:
+                    for word in line.split():
+                        newWord = re.sub(
+                            '[' + string.punctuation + ']', '', word)
+                        if newWord != '':
+                            if word not in words:
+                                # Stop words
+                                if(sys.argv[5] == '--stopwords'):
+                                    words[word] = counter
+                                    counter += 1
+                                elif(sys.argv[5] == '--nostopwords'):
+                                    if word not in stopwords:
                                         words[word] = counter
                                         counter += 1
-                                    elif(sys.argv[5] == '--nostopwords'):
-                                        if word not in stopwords:
-                                            words[word] = counter
-                                            counter += 1
-                                    else:
-                                        print("Invalid argument")
-                                        sys.exit(1)
-            # words_file = open('nopunctnostopWords.txt', 'w')
-            # json.dump(words, words_file)
-            # print("Generation sucsessful")
+                                else:
+                                    print("Invalid argument")
+                                    sys.exit(1)
+        # words_file = open('nopunctnostopWords.txt', 'w')
+        # words_file = open('nopunctWords.txt', 'w')
+        # json.dump(words, words_file)
+        # print("Generation sucsessful")
     else:
         print("Invalid argument")
         sys.exit(1)
