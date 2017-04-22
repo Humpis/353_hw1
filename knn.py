f92b1d6f6127399380364ec1a9a172e1ff4ca7eb
Vidar Minkovsky <vmsailor@gmail.com>
2017-04-22 14:24:24 -0400

    knn done

--- knn.py
+++ knn.py
@@ -1,10 +1,11 @@
 import os
 import sys
-import json
+# import json
 import string
 import re
 import numpy as np
 from numpy import linalg as la
+import scipy.spatial
 path_neg = os.getcwd() + '/review_polarity/txt_sentoken/neg/'
 path_pos = os.getcwd() + '/review_polarity/txt_sentoken/pos/'
 stopwords = ["a", "about", "above", "above", "across", "after",
@@ -206,16 +207,29 @@ else:
     sys.exit(1)
 
 if sys.argv[4] == '--metric=euclidean':
-    metric = 0
+    metric = 'euclidean'
 elif sys.argv[4] == '--metric=manhattan':
-    metric = 1
+    metric = 'manhattan'
 else:
     print("Invalid argument")
     sys.exit(1)
 
+# 5 split
+x1 = np.zeros((400, len(words)), dtype=np.float)
+x2 = np.zeros((400, len(words)), dtype=np.float)
+x3 = np.zeros((400, len(words)), dtype=np.float)
+x4 = np.zeros((400, len(words)), dtype=np.float)
+x5 = np.zeros((400, len(words)), dtype=np.float)
+y1 = np.zeros((400), dtype=np.int)
+y2 = np.zeros((400), dtype=np.int)
+y3 = np.zeros((400), dtype=np.int)
+y4 = np.zeros((400), dtype=np.int)
+y5 = np.zeros((400), dtype=np.int)
+
 xo = x.copy()
 yo = y.copy()
 
+print("Shuffling data matrix")
 for i in range(1, 1000):
     if i % 2 == 0:
         continue
@@ -224,73 +238,197 @@ for i in range(1, 1000):
     xo[i + 999] = x[i]
     yo[i + 999] = y[i]
 
-print('about the do this shit...')
+print('Normalizing feature vectors...')
+for i in range(0, 2000):
+    xi = np.array(xo[i])
+    xinorm = la.norm(xi)
+    xi = xi / xinorm
+    xo[i] = xi
 
-# for i in range(0, 5):
-#     print("ALLAH")
-#     xi = np.array(xo[i])
-#     xinorm = la.norm(xi)
-#     print(xinorm)
-#     print(x[i])
-#     xi = xi / xinorm
-#     xo[i] = xi
-#     print(x[i])
-#     print(xi)
-#     normofX = la.norm(xo[i])
-#     print(normofX)
-#     normofX = la.norm(xi)
-#     print(normofX)
+# 5 split
+for i in range(0, 400):
+    x1[i] = xo[i]
+    x2[i] = xo[i + 400]
+    x3[i] = xo[i + 800]
+    x4[i] = xo[i + 1200]
+    x5[i] = xo[i + 1600]
+    y1[i] = yo[i]
+    y2[i] = yo[i + 400]
+    y3[i] = yo[i + 800]
+    y4[i] = yo[i + 1200]
+    y5[i] = yo[i + 1600]
+
+print("Performing 5-fold cross vaidation...")
+avgPrecisionP = 0
+avgPrecisionN = 0
+avgRecallP = 0
+avgRecallN = 0
+avgAccuracy = 0
+avgPrecision = 0
+avgRecall = 0
 for a in range(0, 5):
     tp = 0
     fp = 0
     tn = 0
     fn = 0
-    for i in range(a * 400, a * 400 + 400):
-        d = np.zeros((2000), dtype=np.float)
-        xi = np.array(xo[i])
-        xinorm = la.norm(xi)
-        xi = xi / xinorm
-        for j in range(0, 2000):
-            if (j >= a * 400) and (j < a * 400 + 400):
-                continue
+    # for i in range(a * 400, a * 400 + 400):
+    #     d = np.zeros((2000), dtype=np.float)
+    #     for j in range(0, 2000):
+    #         if (j >= a * 400) and (j < a * 400 + 400):
+    #             continue
+    #         xi = np.array(xo[i])
+    #         xj = np.array(xo[j])
+    #         # d[j] = la.norm(xi - xj)
+    #         d[j] = scipy.spatial.distance.minkowski(xi, xj, metric)
+    #     sortedd = np.argsort(d)
+    #     positive = 0
+    #     for j in range(0, k):
+    #         positive += yo[sortedd[j + 400]]
+    #     positive /= k
+    #     if positive >= .5:
+    #         if yo[i] == 1:
+    #             tp += 1
+    #         else:
+    #             fp += 1
+    #     else:
+    #         if yo[i] == 1:
+    #             fn += 1
+    #         else:
+    #             tn += 1
+    # this is only good for the first fold. im retartded. but it
+    # also happens to be identical so whatever, it technically gives the right
+    # anwer
 
-            # sumofs = 0
-            # for q in range(0, len(words)):
-            #     sumofs += math.pow(xo[i][q] - xo[j][q], 2)
-            # d[j] = math.sqrt(sumofs)
-
-            xj = np.array(xo[j])
-            xjnorm = la.norm(xj)
-            xj = xj / xjnorm
-
-            d[j] = la.norm(xi - xj)
-        sortedd = np.argsort(d)
-        positive = 0
-        for j in range(0, k):
-            positive += yo[sortedd[j + 400]]
-        positive /= k
-        if positive >= .5:
-            if yo[i] == 1:
-                tp += 1
+    if a == 0:
+        d = scipy.spatial.distance.cdist(x1, xo, metric)
+        d = np.delete(d, [0, 400], axis=1)
+        yy = np.delete(yo, [0, 400], axis=0)
+        for i in range(0, 400):
+            sortedd = np.argsort(d[i])
+            positive = 0
+            for j in range(0, k):
+                positive += yy[sortedd[j]]
+            positive /= k
+            if positive >= .5:
+                if y1[i] == 1:
+                    tp += 1
+                else:
+                    fp += 1
             else:
-                fp += 1
-        else:
-            if yo[i] == 1:
-                fn += 1
+                if y1[i] == 1:
+                    fn += 1
+                else:
+                    tn += 1
+    if a == 1:
+        d = scipy.spatial.distance.cdist(x2, xo, metric)
+        d = np.delete(d, [400, 800], axis=1)
+        yy = np.delete(yo, [400, 800], axis=0)
+        for i in range(0, 400):
+            sortedd = np.argsort(d[i])
+            positive = 0
+            for j in range(0, k):
+                positive += yy[sortedd[j]]
+            positive /= k
+            if positive >= .5:
+                if y2[i] == 1:
+                    tp += 1
+                else:
+                    fp += 1
             else:
-                tn += 1
-    PrecisionP = tp / (tp + fp)
-    PrecisionN = tn / (tn + fn)
-    RecallP = tp / (tp + fn)
-    RecallN = tn / (tn + fp)
-    Accuracy = (tp + tn) / (tp + tn + fp + fn)
+                if y2[i] == 1:
+                    fn += 1
+                else:
+                    tn += 1
+    if a == 2:
+        d = scipy.spatial.distance.cdist(x3, xo, metric)
+        d = np.delete(d, [800, 1200], axis=1)
+        yy = np.delete(yo, [800, 1200], axis=0)
+        for i in range(0, 400):
+            sortedd = np.argsort(d[i])
+            positive = 0
+            for j in range(0, k):
+                positive += yy[sortedd[j]]
+            positive /= k
+            if positive >= .5:
+                if y3[i] == 1:
+                    tp += 1
+                else:
+                    fp += 1
+            else:
+                if y3[i] == 1:
+                    fn += 1
+                else:
+                    tn += 1
+    if a == 3:
+        d = scipy.spatial.distance.cdist(x4, xo, metric)
+        d = np.delete(d, [1200, 1600], axis=1)
+        yy = np.delete(yo, [1200, 1600], axis=0)
+        for i in range(0, 400):
+            sortedd = np.argsort(d[i])
+            positive = 0
+            for j in range(0, k):
+                positive += yy[sortedd[j]]
+            positive /= k
+            if positive >= .5:
+                if y4[i] == 1:
+                    tp += 1
+                else:
+                    fp += 1
+            else:
+                if y4[i] == 1:
+                    fn += 1
+                else:
+                    tn += 1
+    if a == 4:
+        d = scipy.spatial.distance.cdist(x5, xo, metric)
+        d = np.delete(d, [1600, 2000], axis=1)
+        yy = np.delete(yo, [1600, 2000], axis=0)
+        for i in range(0, 400):
+            sortedd = np.argsort(d[i])
+            positive = 0
+            for j in range(0, k):
+                positive += yy[sortedd[j]]
+            positive /= k
+            if positive >= .5:
+                if y5[i] == 1:
+                    tp += 1
+                else:
+                    fp += 1
+            else:
+                if y5[i] == 1:
+                    fn += 1
+                else:
+                    tn += 1
+
+    precisionP = tp / (tp + fp)
+    precisionN = tn / (tn + fn)
+    recallP = tp / (tp + fn)
+    recallN = tn / (tn + fp)
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    precision = (precisionP + precisionN) / 2
+    recall = (recallP + recallN) / 2
     print("Fold: ", a + 1)
-    print("    Precision+ ", PrecisionP)
-    print("    Precision- ", PrecisionN)
-    print("    Recall+    ", RecallP)
-    print("    Recall-    ", RecallN)
-    print("    Accuracy   ", Accuracy)
-    print("    Precision  ", (PrecisionP + PrecisionN) / 2)
-    print("    Recall     ", (RecallP + RecallN) / 2)
+    print("    Precision+ ", precisionP)
+    print("    Precision- ", precisionN)
+    print("    Recall+    ", recallP)
+    print("    Recall-    ", recallN)
+    print("    Accuracy   ", accuracy)
+    print("    Precision  ", precision)
+    print("    Recall     ", recall)
+    avgPrecisionP += precisionP
+    avgPrecisionN += precisionN
+    avgRecallP += recallP
+    avgRecallN += recallN
+    avgAccuracy += accuracy
+    avgPrecision += precision
+    avgRecall += recall
     print(tp, fp, tn, fn)
+print("Average performance measures")
+print("    Precision+ ", avgPrecisionP / 5)
+print("    Precision- ", avgPrecisionN / 5)
+print("    Recall+    ", avgRecallP / 5)
+print("    Recall-    ", avgRecallN / 5)
+print("    Accuracy   ", avgAccuracy / 5)
+print("    Precision  ", avgPrecision / 5)
+print("    Recall     ", avgRecall / 5)
 sys.exit(0)
